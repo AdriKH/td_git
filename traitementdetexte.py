772f94ecc1414ef2bca03a77119e28b310ab356f
Guillaume <uvsq22205129@github.com>
2022-11-15 15:48:00 +0100

    petit chqngement

--- traitementdetexte.py
+++ traitementdetexte.py
@@ -1,1 +1,12 @@
+def upCase(text):
+    text = text.upper()
+    pass
 
+def deleteCaracter(text):
+    for i in range(text.len()):
+        charSpe(text[i])
+    pass
+
+def charSpe(char):
+    if char == "é" or char == "ê":
+        return char = "e"
